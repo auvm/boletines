e54f76481332d45f25b1d0134c8c589050d664ad
auvm <angelurielvelascom@gmail.com>
2023-03-06 13:35:10 -0600

    add fade out animations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,10 +14,64 @@
 // });
 
 
-let celda = document.querySelectorAll("td");
+// let celda = document.querySelectorAll("td");
 
-celda.forEach(function(td){
-    td.addEventListener("click", function(){
-        console.log(this);
-    })
-})
+// celda.forEach(function(td){
+//     td.addEventListener("click", function(){
+//         console.log(this);
+//     })
+// })
+
+
+
+//Obtener elementos del dom
+let links = document.querySelectorAll(".close");
+
+//Recorrer los elementos
+
+links.forEach(function(link){
+    link.addEventListener("click", function(ev){
+        //previene el comportamiento por defecto
+        ev.preventDefault();
+
+        //quitar animaciones de entrada
+        let content = document.querySelector(".content");
+
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeInDown");
+
+        //agregar animaciones de salida
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeOutUp");
+
+        //agrega un timer de 600ms para mostrar las animaciones
+        //luego redirecciona con js a raiz
+        setTimeout(() => {
+           location.href = "/boletines";
+        }, 600);
+    });
+});
+
+let links_more = document.querySelectorAll(".link_more");
+links_more.forEach(function(link){
+    link.addEventListener("click", function(ev){
+
+        ev.preventDefault();
+
+        //quitar animaciones de entrada
+        let content = document.querySelector(".content");
+
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeInDown");
+
+        //agregar animaciones de salida
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeOutUp");
+
+        //agrega un timer de 600ms para mostrar las animaciones
+        //luego redirecciona con js a raiz
+        setTimeout(() => {
+            location.href = link.href;
+        }, 600);
+    });
+});
