5256c28f3c53e6424d89d2d55dd13b768a646c93
auvm <angelurielvelascom@gmail.com>
2023-03-05 23:50:29 -0600

    add basic script for DOM example

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+// console.log("Hello, world!");
+
+
+// let table = document.querySelector(".table");
+
+// console.log(table);
+
+// let links = document.querySelectorAll("a");
+
+// console.log(links);
+
+// links.forEach(function(link){
+//     console.log(link.href);
+// });
+
+
+let celda = document.querySelectorAll("td");
+
+celda.forEach(function(td){
+    td.addEventListener("click", function(){
+        console.log(this);
+    })
+})
